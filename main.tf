7be6e543feafa9b0c39dec563206eac965780062
Abhinav Kumar <22006713+abhinav999k@users.noreply.github.com>
2022-07-13 20:17:22 +0530

    initial commit"

--- main.tf
+++ main.tf
@@ -1,23 +1,34 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.59.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  cloud {
+    organization = "helcrow"
+    workspaces {
+      name = "TerrafromN"
     }
   }
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "azure_rg" {
-  name     =  var.rgname
-  location =  var.location
-}
-
-module "vnet" {
-  source = "./module"
-}
-
-
 provider "azurerm" {
   features {}
+}
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
 }
